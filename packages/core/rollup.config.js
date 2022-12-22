45bbc0f75daa314360e80f5caada2d26a99008b8
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-12-22 20:25:28 +1100

    Minify bundle

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,5 +1,6 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
 import { defineConfig } from 'rollup';
 
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
         nodeResolve({
             exportConditions: ['node'],
         }),
+        terser(),
     ],
     output: {
         file: 'dist/etcher.js',
